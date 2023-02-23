71cc94262642cd98e2a55c19aaafb437230219d1
msyksphinz <msyksphinz.dev@gmail.com>
2023-02-23 14:47:30 +0900

    Update download-env

--- download_env.sh
+++ download_env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -d sniper ]; then
-    git clone git@github.com:msyksphinz-self/sniper.git
+    git clone git@github.com:shioya-lab/sniper.git
 	cd sniper
 	cp -r /home/kimura/work/sniper/pin-3.18-98332-gaebd7b1e6-gcc-linux .
 	ln -s pin-3.18-98332-gaebd7b1e6-gcc-linux pin_kit
@@ -9,15 +9,15 @@ if [ ! -d sniper ]; then
 fi
 
 if [ ! -d rv8 ]; then
-    git clone git@github.com:msyksphinz-self/rv8.git --recurse-submodules -b sift
+    git clone git@github.com:shioya-lab/rv8.git --recurse-submodules -b sift
 fi
 
 if [ ! -d riscv-isa-sim ]; then
-    git clone git@github.com:msyksphinz-self/riscv-isa-sim.git -b sift
+    git clone git@github.com:shioya-lab/riscv-isa-sim.git -b sift
 fi
 
-if [ ! -d sniper_benches ]; then
-    git clone git@github.com:msyksphinz-self/sniper_benches.git
+if [ ! -d vector_benches ]; then
+    git clone git@github.com:shioya-lab/vector_benches.git
 fi
 
 
